8444d1dc6af66dfbbf3cce5952763000816def8f
lokeshwar23bce7214-sketch <lokeshwar.23bce7214@vitapstudent.ac.in>
2026-05-23 18:03:15 +0530

    chore: initialize frontend and backend setup

---
+++ backend/src/test/java/com/tracelm/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tracelm.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
